8077237f5ac832962a9b557e38994cafd0cd8a25
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-03-06 13:20:44 +0100

    lagt till base och layout scss filer. satt en video som bakgrund på sidan. Fått till funktionen att när jag klickar på ett namn på ett dykställe visas det på kartan.

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,37 @@
 //hämtar element från HTML koden
 const inputEl = document.getElementById("search");//inputfält för sök
 const searchBtnEl = document.getElementById("searchBtn");//sökknapp
-const mapEl = document.getElementById("mapContainer");//div container för kartan
+const mapEl = document.getElementById("map");//div container för kartan
+const divespotEl = document.getElementById("info_div");//div element för dykplatsernahämtar
+const headlineEl = document.getElementById("headline_country");
+
+//startkoordinater för kartan vid sidladdning
+const map = L.map('map').setView([58.03222280237399, 14.349985267448993], 12);
+
+//lägger till ett lager på befintlig karta med kartsökning
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//placera markör
+let marker = L.marker([58.03222280237399, 14.349985267448993]).addTo(map);
+
+//"Här är jag" funktion
+L.control.locate().addTo(map);
+
 
 searchBtnEl.addEventListener("click", getData)//eventlistener på sökknappen
+
 
 //async/await funktion
 async function getData() {
 
     const input = inputEl.value;//variabel men värdet från inputfältet på index.html sidan
-
     const url = 'https://world-scuba-diving-sites-api.p.rapidapi.com/api/divesite?country=' + input;//URL för API plus input från sökfältet
-
+   
+    headlineEl.innerHTML = input;//skriver ut input som rubrik på sidan
+    
     //options med mina nycklar och metod
     const options = {
         method: 'GET',
@@ -27,41 +47,24 @@ async function getData() {
         const response = await fetch(url, options);//inväntar svar från fetch
         const result = await response.json();//inväntar svar från konverteringen till javascript
 
-
-        let divespotEl = document.getElementById("info_div");//hämtar div elementet från HTML-koden
+        
         divespotEl.innerHTML = ''; //rensar innehåll
-
-        //Skapar karta och markör. 
-        //startkoordinater för kartan 
-        const map = L.map('map').setView([58.03222280237399, 14.349985267448993], 12);
-
-        //lägger till ett lager på befintlig karta med kartsökning
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-
-        //placera markör
-        let marker = L.marker([58.03222280237399, 14.349985267448993]).addTo(map);
-
-        //"Här är jag" funktion
-        L.control.locate().addTo(map);
 
         //skapar nya element för dykplatserna
         result.data.forEach(diveSite => {
             let siteContainer = document.createElement("div");
-            let regionEl = document.createElement("h2");
             let nameEl = document.createElement("h1");
+            let regionEl = document.createElement("h2");
             let locationEl = document.createElement("p");
 
             //skriver ut info till elementen
-            regionEl.textContent = "Region: " + diveSite.region;
-            nameEl.textContent = "Name: " + diveSite.name;
+            nameEl.textContent = "Divesite: " + diveSite.name;
+            regionEl.textContent = diveSite.region;
             locationEl.textContent = "Location: " + diveSite.location;
 
             //lägg till nya elementen till siteContainer-div elementet
-            siteContainer.appendChild(regionEl);
             siteContainer.appendChild(nameEl);
+            siteContainer.appendChild(regionEl);
             siteContainer.appendChild(locationEl);
 
             //skriv ut till DOM
@@ -92,6 +95,7 @@ async function getData() {
                     .catch(error => {
                         console.error('Det uppstod ett fel:', error);
                     });
+               
             });
         });
 
